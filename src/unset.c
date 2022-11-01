23d16014e921b1cbe824af6d31e0744a848bba4e
Augustine udo Uzochukwu <auzochuk@f0r2s16.codam.nl>
2022-11-01 13:13:44 +0100

    commit

--- src/unset.c
+++ src/unset.c
@@ -2,22 +2,12 @@
 
 int	remove_list(t_env	*env)
 {
-	printf("In remove\n");
-	if(env->beans)
-	{
+	if (env->beans)
 		free(env->beans);
-		printf("Freed beans\n");
-	}
-	if(env->key)
-	{
+	if (env->key)
 		free(env->key);
-		printf("Freed Key \n");
-	}
-	if(env->data)
-	{
+	if (env->data)
 		free(env->data);
-		printf("Freed data\n");
-	}
 	free(env);
 	return (0);
 }
@@ -27,31 +17,27 @@ int ms_unset(t_minishell *shell, t_command *cmd)
 	t_env	*tmp1;
 	t_env	*tmp2;
 
-	if(!cmd->command || !cmd->command[1])
-		return(1);
+	if (!cmd->command || !cmd->command[1])
+		return (1);
 	tmp1 = shell->env;
-	if(ft_strcmp(tmp1->key, cmd->command[1]) == 0)
+	if (ft_strcmp(tmp1->key, cmd->command[1]) == 0)
 	{
 		shell->env = shell->env->next;
-		return(remove_list(tmp1));
+		return (remove_list(tmp1));
 	}
 	while (tmp1 && tmp1->next)
 	{
-		if(ft_strcmp(tmp1->next->key, cmd->command[1]) == 0)
+		if (ft_strcmp(tmp1->next->key, cmd->command[1]) == 0)
 		{
 			tmp2 = tmp1->next;
-			if(tmp2->next)
+			if (tmp2->next)
 				tmp1->next = tmp2->next;
-			if(tmp2)
-				return(remove_list(tmp2));
+			if (tmp2)
+				return (remove_list(tmp2));
 		}
 		tmp1 = tmp1->next;
 	}
 	write(1, cmd->command[1], ft_strlen(cmd->command[1]));
 	ft_putstr_fd(" does not exist\n", 1);
-	return(0);
+	return (0);
 }
-
-		// printf("Tmp1->next = %s\n", tmp1->next->beans);
-			// printf("Tmp2 = %s\n", tmp2->beans);
-			// printf("Tmp1->next after = %s\n", tmp1->next->beans);
