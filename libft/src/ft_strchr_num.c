f17fd3b781393e5a89c97f38af8877e51a833fbe
root <root@Bob.localdomain>
2022-11-09 10:26:31 +0100

    added temporary strchr_num, need to push real one from codam

---
+++ libft/src/ft_strchr_num.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strchr_num.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: wkonings <root@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/09 10:23:30 by root          #+#    #+#                 */
+/*   Updated: 2022/11/09 10:25:54 by root          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+
+//temporary file, need to push the real one from codam.
+int ft_strchr_num(const char *s, int c)
+{
+    int i;
+
+    i = -1;
+    while (s[++i])
+        if (s[i] == (char)c)
+            return (i);
+    return (-1);
+}
